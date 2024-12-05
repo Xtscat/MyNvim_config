f672ca375162f6be33fcb8226ee58b69178f1672
Xtscat <1508957301@qq.com>
2024-12-05 20:45:29 +0800

    Automatically switch themes based on local time

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,17 +1,39 @@
 return {
     {
+        'JManch/sunset.nvim',
+        dependencies = {
+            "catppuccin/nvim",
+        },
+        lazy = false,
+        priority = 1000,
+        config = function()
+            local catppuccin = require('catppuccin').setup
+            require('sunset').setup({
+                latitude = 36.31,
+                longitude = 104.48,
+                day_callback = function()
+                    if vim.g.is_day then
+                        catppuccin.flavour = 'latte'
+                    else
+                        catppuccin.flavour = 'mocha'
+                    end
+                end
+            })
+        end
+    },
+    {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
         config = function()
             require("catppuccin").setup({
-                -- flavour = "auto", -- latte, frappe, macchiato, mocha
-                flavour = "mocha", -- latte, frappe, macchiato, mocha
-                background = {      -- :h background
+                flavour = "auto", -- latte, frappe, macchiato, mocha
+                -- flavour = "mocha", -- latte, frappe, macchiato, mocha
+                background = {    -- :h background
                     light = "latte",
                     dark = "mocha",
                 },
-                transparent_background = true, -- disables setting the background color.
+                transparent_background = false, -- disables setting the background color.
                 show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
                 term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
                 dim_inactive = {
@@ -37,7 +59,43 @@ return {
                     operators = {},
                     -- miscs = {}, -- Uncomment to turn off hard-coded styles
                 },
-                color_overrides = {},
+                color_overrides = {
+                    {
+                        -- this 16 colors are changed to onedark
+                        base = "#282c34",
+                        mantle = "#353b45",
+                        surface0 = "#3e4451",
+                        surface1 = "#545862",
+                        surface2 = "#565c64",
+                        text = "#abb2bf",
+                        rosewater = "#b6bdca",
+                        lavender = "#c8ccd4",
+                        red = "#e06c75",
+                        peach = "#d19a66",
+                        yellow = "#e5c07b",
+                        green = "#98c379",
+                        teal = "#56b6c2",
+                        blue = "#61afef",
+                        mauve = "#c678dd",
+                        flamingo = "#be5046",
+
+                        -- now patching extra palettes
+                        maroon = "#e06c75",
+                        sky = "#d19a66",
+
+                        -- extra colors not decided what to do
+                        pink = "#F5C2E7",
+                        sapphire = "#74C7EC",
+
+                        subtext1 = "#BAC2DE",
+                        subtext0 = "#A6ADC8",
+                        overlay2 = "#9399B2",
+                        overlay1 = "#7F849C",
+                        overlay0 = "#6C7086",
+
+                        crust = "#11111B",
+                    }
+                },
                 custom_highlights = {},
                 default_integrations = true,
                 integrations = {
