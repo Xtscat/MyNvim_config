eedaf91de900687b798d45034aef2251f9d65112
Xtscat <1508957301@qq.com>
2025-10-12 17:28:49 +0800

    add cmake-tools.nvim

--- lua/plugins/code_runner.lua
+++ lua/plugins/code_runner.lua
@@ -1,31 +1,4 @@
 return {
-    -- {
-    --     "CRAG666/code_runner.nvim",
-    --     event = "VeryLazy",
-    --     config = function()
-    --         require("code_runner").setup {
-    --             vim.keymap.set('n', '<leader>rc', '<cmd>RunCode<cr>', { noremap = true, silent = false }, { desc = "[R]un[C]ode" }),
-    --             mode = "term",
-    --             filetype = {
-    --                 c = {
-    --                     "cd $dir &&",
-    --                     "gcc $fileName -o",
-    --                     "/tmp/$fileNameWithoutExt &&",
-    --                     "/tmp/$fileNameWithoutExt &&",
-    --                     "rm /tmp/$fileNameWithoutExt",
-    --                 },
-    --                 cpp = {
-    --                     "cd $dir &&",
-    --                     "g++ $fileName",
-    --                     "-o /tmp/$fileNameWithoutExt &&",
-    --                     "/tmp/$fileNameWithoutExt",
-    --                 },
-    --                 python = "python3 -u '$dir/$fileName'",
-    --                 bash = "bash '$dir/$fileName'"
-    --             }
-    --         }
-    --     end
-    -- }
     { -- This plugin
         "Zeioth/compiler.nvim",
         cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
