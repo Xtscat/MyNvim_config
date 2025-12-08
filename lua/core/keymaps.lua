e7acc6b7190395616b5bf0763c691e204ac38827
Xtscat <1508957301@qq.com>
2025-12-08 12:02:42 +0800

    adjust ctrl-u/d to 1/4 screen and map to ctrl-j/k

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -15,6 +15,17 @@ vim.keymap.set("v", "L", "$", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>l", "<cmd>echo expand('%:p')<cr>")
 
+-- 将ctrl-d/u修改为滚动1/4屏幕, 并映射到ctrl-j/k
+local function move_quarter(direction)
+    local win_height = vim.api.nvim_win_get_height(0)
+    local step = math.max(1, math.floor(win_height / 4))
+    vim.cmd("normal! m'")
+    vim.cmd("normal!" .. step .. direction)
+end
+
+vim.keymap.set({ "n", "x" }, "<C-j>", function() move_quarter("j") end, { desc = "Down 1/4 screen" })
+vim.keymap.set({ "n", "x" }, "<C-k>", function() move_quarter("k") end, { desc = "Up 1/4 screen" })
+
 -- vim.api.nvim_create_autocmd("VimEnter", {
 --     callback = function()
 --         vim.cmd("Neotree show")
